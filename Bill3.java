e0becdc47e3680b1304c5afc81801a3bd22a693d
יואב <yoav@MacBook-Air-sl-ywb.local>
2025-10-30 18:53:52 +0200

    fixes

--- Bill3.java
+++ Bill3.java
@@ -5,7 +5,7 @@ public class Bill3 {
 	    String name1 = args[0];
 	    String name2 = args[1];
 	    String name3 = args[2];
-		int bill = Integer.parseInt(args[3]);
+		double bill = Double.parseDouble(name3)args[3]);
 		double each_bill = bill/3;
 		System.out.println("Dear " + name3 + ", " + name2 + ", and " + name1 + 
 		": pay "+ Math.ceil(each_bill) + " Shekels each");
